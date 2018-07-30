0513345fcd8fd28c942f55641308a5b2a7bd42b7
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-07-30 08:59:06 +0200

    Use callbag composition instead of custom implementation

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-const mapWhen = (f, m) => (source) => (start, sink) => {
-  if (start !== 0) return;
-  let ask;
-  source(0, (t, d) => {
-    if (t === 0) ask = d;
-    if (t === 1) {
-      f(d) ? sink(t, m(d)) : ask(t);
-    } else {
-      sink(t, d);
-    }
-  });
-};
+const filter = require('callbag-filter');
+const map = require('callbag-map');
+
+const mapWhen = (f, m) => source => map(m)(filter(f)(source));
 
 module.exports = mapWhen;
